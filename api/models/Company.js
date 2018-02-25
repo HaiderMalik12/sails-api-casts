b10e453b55f76b7a4ee12e40828581b2a9c59d56
Haider Malik <haidermalik504@gmail.com>
2018-02-25 14:01:13 +0500

    created company model

---
+++ api/models/Company.js
@@ -0,0 +1,14 @@
+module.exports = {
+  attributes: {
+    name: {
+      type: 'string',
+      required: true
+    },
+    city: {
+      type: 'string'
+    },
+    address: {
+      type: 'string'
+    }
+  }
+};
